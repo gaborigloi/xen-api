c64725366bbf7e0f41cae718bc17203c8acadc05
Iglói Gábor <gabor.igloi@citrix.com>
2017-10-07 13:39:05 +0100

    ounit_comparators.ml: add ref comparator
    Signed-off-by: Iglói Gábor <gabor.igloi@citrix.com>

--- ocaml/xapi/ounit_comparators.ml
+++ ocaml/xapi/ounit_comparators.ml
@@ -6,5 +6,13 @@ struct
   let pp_print_sep = OUnitDiff.pp_comma_separator
 end
 
+module RefDiff(RefType : sig type t end) = struct
+  type t = RefType.t Ref.t
+  let compare = compare
+  let pp_printer formatter ref = Format.pp_print_string formatter (Ref.string_of ref)
+  let pp_print_sep = OUnitDiff.pp_comma_separator
+end
+
 module StringSet = OUnitDiff.SetMake(StringDiff)
 module StringList = OUnitDiff.ListSimpleMake(StringDiff)
+module RefList(RefType : sig type t end) = OUnitDiff.ListSimpleMake(RefDiff(RefType))
